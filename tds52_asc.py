c296635915802d17196f6e1bf90886aec9de8377
bayu imbang <imbang80@gmail.com>
2013-05-23 20:33:35 +0700

    3rd commit

---
+++ tds52_asc.py
@@ -0,0 +1,15 @@
+fid=open('C:\coba_N.txt','r')
+fid.readline()
+#'!header file automatically created by program ADAPT\n'
+fid.readline()
+#'!* 29-Jun-2012_09:06:51.000 \n'
+fid.readline()
+#'!* width 1749.000 sec \n'
+fid.readline()
+#'1\n'
+fid.readline()
+#'\n'
+fid.readline()
+#'\n'
+fid.readline()
+#'Data nDirect = NS\n'
